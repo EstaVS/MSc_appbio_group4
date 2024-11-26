a12a59da703f76d3ed865d3305e59ab19a7eec2c
Era Aziz <k24088153@erc-hpc-comp015.create.kcl.ac.uk>
2024-11-26 15:27:36 +0000

    Environment script

---
+++ script/environment.sh
@@ -0,0 +1,22 @@
+# In the nano script
+
+#!/bin/bash
+
+echo "start of the pipeline"
+
+# Add conda channels
+conda config --add channels bioconda
+conda config --add channels conda-forge
+
+# Create a new conda environment with specific versions
+conda create -n bioenv cutadapt=2.3 faqcs=2.08 star= 2.5.2a fdrtool= 1.2.15 -y
+
+#Load Fastqc
+
+module load fastqc
+
+# Print a message about how to activate the environment
+echo "To activate your environment, run: conda activate bioenv" 
+
+
+echo "end of the pipeline"
