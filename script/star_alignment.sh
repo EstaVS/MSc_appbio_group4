846fb7f05a0b0804461aeadc5901f9d6ff487086
Siwanart Ma <k24023654@erc-hpc-comp015.create.kcl.ac.uk>
2024-11-29 17:40:31 +0000

    modify star_alignment.sh according to protocol

--- script/star_alignment.sh
+++ script/star_alignment.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Parameters - customize these paths
+INDEX_PATH="../../index/SAindex"
 REF_GENOME_DIR="/scratch_tmp/grp/msc_appbio/group4_tmp/ref_genome/GCA_000269885.1_ASM26988v1_genomic.fna"    # Path to STAR indexed genome
 FASTQ="../../raw_data_rna/fastq/ERR4553381.fastq.gz"
-OUTPUT_DIR="../../star_output"                     # Directory for STAR output
+OUTPUT_DIR="../../star_output_test381"                     # Directory for STAR output
 THREADS=4                                    # Number of threads for STAR
 
 # load star
@@ -14,15 +15,18 @@ mkdir -p $OUTPUT_DIR
 
 # Run STAR
 echo "Running STAR alignment..."
-STAR \
-        --genomeDir $REF_GENOME_DIR \
-        --readFilesIn $FASTQ \
-        --readFilesCommand zcat \
-        --runThreadN $THREADS \
-        --outFileNamePrefix ${OUTPUT_DIR}/ \
-        --outSAMtype BAM SortedByCoordinate \
-        --outSAMattributes Standard \
-        --quantMode TranscriptomeSAM GeneCounts\
-        --peOverlapNbasesMin 10
+
+STAR --genomeDir $INDEX_PATH \
+    --readFilesIn $FASTQ \
+    --readFilesCommand zcat --runThreadN 4 \
+    --genomeLoad NoSharedMemory --outFilterMultimapNmax 20 \
+    --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 \
+    --outFilterMismatchNmax 999 --outFilterMismatchNoverReadLmax 0.04 \
+    --alignIntronMin 20 --alignIntronMax 1000000 \
+    --alignMatesGapMax 1000000 --outSAMheaderHD @HD VN:1.4 SO:coordinate \
+    --outSAMunmapped Within --outFilterType BySJout --outFileNamePrefix ${OUTPUT_DIR} \
+    --outSAMattributes NH HI AS NM MD --outSAMstrandField intronMotif \
+    --outSAMtype BAM SortedByCoordinate --quantMode TranscriptomeSAM GeneCounts \
+    --sjdbScore 1 --limitBAMsortRAM 8000000000 \
 
 echo "Alignment completed. Output saved to ${OUTPUT_DIR}/"
