ab297a55cc8bbb5c8c27d5ca5463d735c5f8ac78
Siwanart Ma <k24023654@erc-hpc-login2.create.kcl.ac.uk>
2024-11-29 22:59:12 +0000

    make star_alignment.sh into sbatch script for all fastq.gz files

--- script/star_alignment.sh
+++ script/star_alignment.sh
@@ -13,8 +13,8 @@
 # Parameters - customize these paths
 INDEX_PATH="../../index/"  # Path to STAR indexed genome
 REF_GENOME_DIR="/scratch_tmp/grp/msc_appbio/group4_tmp/ref_genome/GCA_000269885.1_ASM26988v1_genomic.fna"    # Path to reference genome
-FASTQ="../../raw_data_rna/fastq/ERR4553381.fastq.gz"
-OUTPUT_DIR="../../star_output_test381"                     # Directory for STAR output
+FASTQ_DIR="../../raw_data_rna/fastq/"
+OUTPUT_DIR="../../star_output"                     # Directory for STAR output
 THREADS=4                                    # Number of threads for STAR
 
 # load star
@@ -23,34 +23,36 @@ module load star/2.7.10b-gcc-13.2.0
 # Create output directory if it doesn't exist
 mkdir -p $OUTPUT_DIR
 
-# Run STAR
-echo "Running STAR alignment..."
+# Loop through each fastq.gz file in the raw_data_rna/fastq directory
+for FASTQ in $FASTQ_DIR/*.fastq.gz;do
+	BASENAME=$(basename $FASTQ .fastq.gz)
 
-# Run STAR alignment
-echo "Running STAR alignment..."
-STAR --genomeDir $INDEX_PATH \
-    --readFilesIn $FASTQ \
-    --readFilesCommand zcat \
-    --runThreadN $THREADS \
-    --genomeLoad NoSharedMemory \
-    --outFilterMultimapNmax 20 \
-    --alignSJoverhangMin 8 \
-    --alignSJDBoverhangMin 1 \
-    --outFilterMismatchNmax 999 \
-    --outFilterMismatchNoverReadLmax 0.04 \
-    --alignIntronMin 20 \
-    --alignIntronMax 1000000 \
-    --alignMatesGapMax 1000000 \
-    --outSAMheaderHD @HD VN:1.4 SO:coordinate \
-    --outSAMunmapped Within \
-    --outFilterType BySJout \
-    --outFileNamePrefix ${OUTPUT_DIR}/ \
-    --outSAMattributes NH HI AS NM MD \
-    --outSAMstrandField intronMotif \
-    --outSAMtype BAM SortedByCoordinate \
-    --quantMode TranscriptomeSAM GeneCounts \
-    --sjdbScore 1 \
-    --limitBAMsortRAM 8000000000
+	# Run STAR
+	echo "Running STAR alignment for $BASENAME..."
 
+	STAR --genomeDir $INDEX_PATH \
+    	--readFilesIn $FASTQ \
+   	--readFilesCommand zcat \
+   	 --runThreadN $THREADS \
+   	 --genomeLoad NoSharedMemory \
+   	 --outFilterMultimapNmax 20 \
+   	 --alignSJoverhangMin 8 \
+   	 --alignSJDBoverhangMin 1 \
+   	 --outFilterMismatchNmax 999 \
+   	 --outFilterMismatchNoverReadLmax 0.04 \
+   	 --alignIntronMin 20 \
+   	 --alignIntronMax 1000000 \
+   	 --alignMatesGapMax 1000000 \
+   	 --outSAMheaderHD @HD VN:1.4 SO:coordinate \
+   	 --outSAMunmapped Within \
+   	 --outFilterType BySJout \
+   	 --outFileNamePrefix ${OUTPUT_DIR}/ \
+   	 --outSAMattributes NH HI AS NM MD \
+   	 --outSAMstrandField intronMotif \
+   	 --outSAMtype BAM SortedByCoordinate \
+	--quantMode TranscriptomeSAM GeneCounts \
+    	--sjdbScore 1 \
+    	--limitBAMsortRAM 8000000000
 
-echo "Alignment completed. Output saved to ${OUTPUT_DIR}/"
+	echo "Alignment completed for $BASENAME. Output saved to ${OUTPUT_DIR}/${BASENAME}_"
+done
