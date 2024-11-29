2238b2ea8849536a912e2c6cb52dcf9b40e45bda
Era Aziz <k24088153@erc-hpc-login2.create.kcl.ac.uk>
2024-11-29 16:00:14 +0000

    Adding htseq to our conda environment for count reads

--- script/environment.sh
+++ script/environment.sh
@@ -7,7 +7,7 @@ conda config --add channels bioconda
 conda config --add channels conda-forge
 
 # Create a new conda environment with specific versions
-conda create -n bioenv cutadapt=2.3 faqcs=2.08 star= 2.5.2a fdrtool= 1.2.15 -y
+conda create -n bioenv cutadapt=2.3 faqcs=2.08 star= 2.5.2a fdrtool= 1.2.15 htseq 0.9.1 -y
 
 #Load Fastqc
 
