1f01d4d6543fc980fcc39758fa0920d0b4af9107
Esta Shrewsbury <k24058218@erc-hpc-comp015.create.kcl.ac.uk>
2024-12-14 20:22:41 +0000

    - Added Differential Expression analysis for Strain type - Cleaned up Script for final presentation - Added PCA Loadings graph (low priority)

--- script/DESeq2_script.R
+++ script/DESeq2_script.R
@@ -4,7 +4,7 @@ setwd("~/Desktop/working_directory/AppBio_project/")
 
 #BiocManager::install("DESeq2") 
 #BiocManager::install(c("GenomicFeatures", "txdbmaker"))
-
+#install.packages("fdrtool")
 
 library(DESeq2)
 library(GenomicFeatures)
@@ -21,31 +21,23 @@ count_file_list
 metadata <- read.csv(file ="phenotype_metadata.csv", header = TRUE, row.names = 1)
 head(metadata)
 
-### 
-# This section cannot be done on HPC as GenomicFeatures is not available 
-###
+### Creating database from GTF file to extract gene names
 
 # Load GTF file
-# txdb <- makeTxDbFromGFF(file = "genomic.gtf", format = "gtf")
-# txdb
+txdb <- makeTxDbFromGFF(file = "genomic.gtf", format = "gtf")
+txdb
 
 # Extracting gene data for gene reference list
-# genes <- genes(txdb)
-# genes_df <- as.data.frame(genes)
+genes <- genes(txdb)
+genes_df <- as.data.frame(genes)
 
 # Extract only the gene IDs
-# gene_ids <- rownames(genes_df)
+reference_genes <- rownames(genes_df)
 
 # View the first few gene IDs
-# head(gene_ids)
+head(reference_genes)
 
-# Save the gene list to a file
-# write.table(gene_ids, file = "gene_ids.txt", quote = FALSE, row.names = FALSE, col.names = FALSE)
-
-### End of GenomicFeatures section
-
-reference_genes <- read.table("gene_ids.txt", header = FALSE, stringsAsFactors = FALSE)
-reference_genes <- reference_genes$V1  # Extract the gene names
+###
 
 # Function to Align Counts to Reference Genes
 read_and_align <- function(file, reference_genes) {
@@ -83,56 +75,98 @@ all(rownames(metadata) == colnames(count_matrix))
 # Check for NA values
 sum(is.na(count_matrix)) # None
 
-###
+### Differential Expression according to Phenotype
 
-dds <- DESeqDataSetFromMatrix(countData = count_matrix,
+dds_p <- DESeqDataSetFromMatrix(countData = count_matrix,
                               colData = metadata,
                               design = ~ phenotype) 
 
-dds <- DESeq(dds)
 # Performs normalization and fits the model
+dds_p <- DESeq(dds_p)
 
-results <- results(dds) # Automatically performs independent filtering
-head(results)
+results_p <- results(dds_p) # Automatically performs independent filtering
+head(results_p)
 
 # Check for NA values
-sum(is.na(results)) # 131 NA values
+sum(is.na(results_p)) # 131 NA values
 # Where are they?
-sum(is.na(results$pvalue)) # 5 NAs in p-value
+sum(is.na(results_p$pvalue)) # 5 NAs in p-value
 
 # Excluding NA values from analysis
-exNA_results <- results[!is.na(results$pvalue), ]
-head(exNA_results)
-sum(is.na(exNA_results)) # 106 NAs
-sum(is.na(exNA_results$padj)) # Also 106 NAs therefore no more need to be excluded
+exNA_results_p <- results_p[!is.na(results_p$pvalue), ]
+head(exNA_results_p)
 
-# fdrtools section - Multiple testing correction
+sum(is.na(exNA_results_p)) # 106 NAs
 
-#install.packages("fdrtool")
+### Differential Expression according to Strain
+
+dds_s <- DESeqDataSetFromMatrix(countData = count_matrix,
+                              colData = metadata,
+                              design = ~ strain) 
+# Performs normalization and fits the model
+dds_s <- DESeq(dds_s) 
+
+results_s <- results(dds_s)
+head(results_s)
+
+# Check for NA values
+sum(is.na(results_s)) # 25 NA values
+# Where are they?
+sum(is.na(results_s$pvalue)) # 5 NAs in p-value
+
+# Excluding NA values from analysis
+exNA_results_s <- results_s[!is.na(results_s$pvalue), ]
+head(exNA_results_s)
+
+sum(is.na(exNA_results_s)) # 0 NAs ???
+
+### fdrtools section - Multiple testing correction
+
 library(fdrtool)
 
-pvalues <- exNA_results$pvalue
+# Phenotype differential expression
+
+pvalues_p <- exNA_results_p$pvalue
   
-fdr_results <- fdrtool(pvalues, statistic = "pvalue")
-summary(fdr_results)
+fdr_results_p <- fdrtool(pvalues_p, statistic = "pvalue")
+summary(fdr_results_p)
 
 # Add q-values to the original results table
-exNA_results$qval <- fdr_results$qval
+exNA_results_p$qval <- fdr_results_p$qval
 
-head(exNA_results)
+head(exNA_results_p)
 
-significant_genes <- subset(exNA_results, qval < 0.1)
-head(significant_genes)
+significant_genes_p <- subset(exNA_results_p, qval < 0.1)
+head(significant_genes_p)
 
-###
+summary(significant_genes_p)
 
-write.csv(as.data.frame(exNA_results), file = "differential_expression_results.csv")
-write.csv(as.data.frame(significant_genes), file = "significant_genes_results.csv")
+# Writing to a csv file
+write.csv(as.data.frame(significant_genes_p), file = "significant_genes_phenotype.csv")
+
+# Strain differential expression
+
+pvalues_s <- exNA_results_s$pvalue
+
+fdr_results_s <- fdrtool(pvalues_s, statistic = "pvalue")
+summary(fdr_results_s)
+
+# Add q-values to the original results table
+exNA_results_s$qval <- fdr_results_s$qval
+
+head(exNA_results_s)
+
+significant_genes_s <- subset(exNA_results_s, qval < 0.1)
+head(significant_genes_s)
+
+summary(significant_genes_s)
+
+# Writing to a csv file
+write.csv(as.data.frame(significant_genes_s), file = "significant_genes_strain.csv")
 
 ### PCA plot ###
 
 library(ggplot2)
-#install.packages("ggfortify")
 library(ggfortify)
 
 # Using count_matrix for PCA data
@@ -149,14 +183,10 @@ filtered_matrix <- count_matrix[apply(count_matrix, 1, function(x) var(x) > 0), 
 pca <- prcomp(t(filtered_matrix), scale. = TRUE)
 summary(pca)
 
-# Showing string of phenotype metadata and strain metadata - just to check names
-metadata$phenotype
-metadata$strain
-
 # Plotting PCA
 PCAplot <- ggplot(pca, aes(x = PC1, y = PC2, colour = metadata$phenotype, shape = metadata$strain)) +
          geom_point(size = 3) +
-         labs(title = "Transcriptomics PCA Plot", x = "PC1: 35.5% variance", y = "PC2: 28.6% variance", colour = "Phenotype", shape = "Strain Type") +
+         labs(title = "Transcriptomics PCA Plot", x = "PC1: 46.07% variance", y = "PC2: 13.15% variance", colour = "Phenotype", shape = "Strain Type") +
          scale_color_manual(values = c("fumarate_producer" = "indianred2", "malate_producer" = "olivedrab3", "succinate_producer" = "turquoise3", "wild_type" = "orchid2"), labels = c("Fumarate Producer", "Malate Producer", "Succinate Producer", "Wild Type")) +
          scale_shape_manual(values = c("parental" = 15, "evolved" = 16, "wild_type" = 17), labels = c("Parental", "Evolved", "Wild Type")) +
          theme_bw() + 
@@ -164,3 +194,26 @@ PCAplot <- ggplot(pca, aes(x = PC1, y = PC2, colour = metadata$phenotype, shape 
 
 # Saving plot as image
 ggsave("PCAplot.png", plot = PCAplot, width = 12, height = 8)
+
+### This section is not included in the Rmarkdown file
+
+# Create a loading plot
+library(ggforce)
+
+loadings <- pca$rotation
+print(loadings)
+
+# Convert loadings to a data frame for ggplot2
+loadings_df <- as.data.frame(loadings)
+loadings_df$Variable <- rownames(loadings_df)
+
+# Plot loadings
+ggplot(loadings_df, aes(x = PC1, y = PC2, label = Variable)) +
+  geom_point() +
+  geom_segment(aes(x = 0, y = 0, xend = PC1, yend = PC2), 
+               arrow = arrow(length = unit(0.2, "cm")), color = "blue") +
+  labs(title = "Loading Plot", x = "PC1", y = "PC2") +
+  theme_minimal() +
+  geom_circle(aes(x0 = 0, y0 = 0, r = 1), linetype = "dashed", inherit.aes = FALSE)
+# Plot shows minimal contribution by variables to PC1 & PC2
+# This is typical for transcriptomic analysis as the data shows high-dimensionality
